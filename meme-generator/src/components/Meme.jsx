cba2f00166052cab472aed0026383f3e6e65be97
Parth <parthp15@yahoo.com>
2023-09-07 19:24:36 -0700

    added memesData

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,4 +1,16 @@
+import {useState} from "react"
+import memesData from "../memesData.js"
+
 export default function Meme() {
+    const [memeImage, setMemeImage] = useState("")
+    
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        
+    }
+    
     return (
         <main>
             <div className="form">
@@ -14,10 +26,12 @@ export default function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
             </div>
+            <img src={memeImage} className="meme--image" />
         </main>
     )
 }
