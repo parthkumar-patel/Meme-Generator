6f1ec0509ba6d62e08d576f276076b70b6883bc6
Parth <parthp15@yahoo.com>
2023-09-09 22:18:30 -0700

    fetching the data from an api

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,5 +1,4 @@
-import {useState} from "react"
-import memesData from "../memesData"
+import {useState, useEffect} from "react"
 
 export default function Meme() {
     const [meme, setMeme] = useState({
@@ -7,17 +6,31 @@ export default function Meme() {
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-    
+    const [allMemes, setAllMemes] = useState([])
+        
+    useEffect(() => {
+        //Version 1 using async
+        //
+        // async function getMemes() {
+        //     const res = await fetch("https://api.imgflip.com/get_memes")
+        //     const data = await res.json()
+        //     setAllMemes(data.data.memes)
+        // }
+        // getMemes()
+
+        // Version 2 
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
     
     function getMemeImage() {
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
         }))
-        
     }
     
     function handleChange(event) {
